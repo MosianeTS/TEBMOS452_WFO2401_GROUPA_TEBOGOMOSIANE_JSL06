e7ff8b6f6dab10371e571f719c292a085b148ceb
Tebogo Mosiane <mosianets@gmail.com>
2024-03-26 13:28:39 +0200

    Initial commit, displayed menu categories on browser

---
+++ practice.js
@@ -0,0 +1,9 @@
+
+
+let netflixSeries = [
+    ["Stranger Things", 2016, ["Drama", "Fantasy", "Horror"]],
+    ["The Crown", 2016, ["Drama", "History"]],
+    ["Black Mirror", 2011, ["Drama", "Sci-fi", "Thriller"]]
+];
+
+console.log(netflixSeries[0][2][1])
